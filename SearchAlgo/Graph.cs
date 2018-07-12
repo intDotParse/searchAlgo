bf8c99d2a7fdd37adebdd5b5b9d4d377d8399b4d
Artemio Cabrillos JR <act.acabrillos@gmail.com>
2018-07-12 09:39:53 +0800

    search algo testing

--- SearchAlgo/Graph.cs
+++ SearchAlgo/Graph.cs
@@ -8,22 +8,35 @@ namespace SearchAlgo
 {
     class Graph
     {
-        public Node node;
+        public Node[] nodes;
         public Graph()
         {
 
         }
+
     }
     class Node
     {
         private int nodeId;
         private int x;
         private int y;
+        private Node[] neighbors;
+        private int neighborCounter = 0;
         public Node(int nodeId, int x, int y)
         {
             this.name = nodeId;
             this.x = x;
             this.y = y;
+        }
+
+        public void setNeighbors(Node neighbor)
+        {
+            neighbors[neighborCounter] = neighbor;
+            neighborCounter++;
+        }
+        public Node[] getAllNeigbors()
+        {
+            return neighbors;
         }
         
     }
